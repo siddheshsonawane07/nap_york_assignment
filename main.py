23c8085e9678a11d35204867ca77b87d7e248f50
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2024-10-06 13:27:17 +0530

    changes

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
         repository_analytics.run()
 
     st.markdown("<hr style='border-top: 2px solid #FFA500;'>", unsafe_allow_html=True)
-    st.markdown("<p style='text-align: center; color: #D3D3D3;'>Built with ❤ by Siddhesh</p>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: center; color: #D4D3D3;'>Built with ❤ by Siddhesh</p>", unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
